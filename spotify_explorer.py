305a299f85ea4e98c2d3e4363a6b209b0f4e031a
davr <davr@davr.org>
2024-01-08 16:05:33 -0800

    Add spotify auth all in-browser. Add some exception catching. Added artist search, but results are not used yet.

--- spotify_explorer.py
+++ spotify_explorer.py
@@ -1,90 +1,107 @@
-import pandas as pd
 import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from nicegui import ui
+from nicegui import ui, app
+from spotipy.cache_handler import CacheHandler
+
+class NiceguiCache(CacheHandler):
+    def get_cached_token(self):
+        if 'spotipy_token' in app.storage.user:
+            return app.storage.user['spotipy_token']
+        else:
+            return None
+
+    def save_token_to_cache(self, token):
+        print(token)
+        app.storage.user['spotipy_token'] = token
+    
+    @staticmethod
+    def clear_cache():
+        app.storage.user['spotipy_token'] = None
 
 device_id=None
 
-@ui.page('/run')
-def page():
+def artists_to_df(artists):
+    arts = []
+    for i in artists:
+        try:
+            img = i['images'][-1]['url']
+        except:
+            img = ''
+        art = {
+        'img': '<img src="%s" width="30">'%img,
+        'use': False,
+        'artist': i['name'],
+        'artistid': i['id'],
+        }
+        arts += [art]
+    return arts    
+
+def tracks_to_df(tracks):
+    trax = []
+    for track in tracks:
+        trx = {
+        'img':'<img src="%s" width="30">'%track['album']['images'][-1]['url'],
+        'use':False,
+        'track':track['name'],
+        'trackid':track['id'],
+        'artist':track['artists'][0]['name'],
+        'artistid':track['artists'][0]['id'],
+        'album':track['album']['name'],
+        'albumid':track['album']['id'],
+        'tracknum':track['track_number'],
+        'releasedate':track['album']['release_date'],
+        'duration':track['duration_ms']/1000,
+        }
+        trax += [trx]
+    return trax
+
+@ui.page('/auth')
+async def page(client):
     print("Loading")
 
-    def get_spot():
-        scope = "user-read-playback-state,user-modify-playback-state,user-read-currently-playing,app-remote-control,streaming"
-        spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-        return spotify
+    scope = "user-read-playback-state,user-modify-playback-state,user-read-currently-playing,app-remote-control,streaming"
+    sp_auth = SpotifyOAuth(scope=scope, cache_handler=NiceguiCache(), open_browser=False)
+    token_info = sp_auth.get_cached_token()
+    access_token = None
+    # We have a cached token
+    if token_info:
+        access_token = token_info['access_token']
+    else:  # check if a token got passed in the URL
+        await client.connected()
+        url = await ui.run_javascript('window.location.href')
+        print(url)
+        code = sp_auth.parse_response_code(url)
+        if code != url:
+            token = sp_auth.get_access_token(code)
+            access_token = token['access_token']
+            ui.open("/auth", new_tab=False)
+    
+    # No token, redirecto to spotify
+    if not access_token:
+        ui.button("Authorize Spotify", on_click=lambda ev:ui.open(sp_auth.get_authorize_url(), new_tab=False))
+        return
+    
+    spotify = spotipy.Spotify(access_token)
+    me = spotify.me()
+    print(me)
 
-    def search_track(query):
-        global spotify
-        res = spotify.search(q=query, type='track')
-        print("Spotify returned %d items" % len(res['tracks']['items']))
-        return res
-
-    def search_artist(query):
-        global spotify
-        res = spotify.search(q=query, type='artist')
-        print("Artist returned %d items" % len(res['artists']['items']))
-
-    def artists_to_df(artists):
-        arts = []
-        for i in artists:
-            try:
-                img = i['images'][-1]['url']
-            except:
-                img = ''
-            art = {
-            'img': '<img src="%s" width="30">'%img,
-            'use': False,
-            'artist': i['name'],
-            'artistid': i['id'],
-            }
-            arts += [art]
-        return arts    
-
-    def tracks_to_df(tracks):
-        trax = []
-        for track in tracks:
-            trx = {
-            'img':'<img src="%s" width="30">'%track['album']['images'][-1]['url'],
-            'use':False,
-            'track':track['name'],
-            'trackid':track['id'],
-            'artist':track['artists'][0]['name'],
-            'artistid':track['artists'][0]['id'],
-            'album':track['album']['name'],
-            'albumid':track['album']['id'],
-            'tracknum':track['track_number'],
-            'releasedate':track['album']['release_date'],
-            'duration':track['duration_ms']/1000,
-            }
-            trax += [trx]
-        return trax
-
-    def get_reccs(limit, artists, tracks):
-        reccs =  spotify.recommendations(
-            limit=limit,
-            seed_artists=artists,
-            seed_tracks=tracks,
-            seed_genre=None
-            )
-        print("Spotify rec %d" % len(reccs['tracks']))    
-        return reccs
-
-    tracks_df = None
+    def logout():
+        NiceguiCache.clear_cache()
+        ui.open("/auth", new_tab=False)
 
     def onsearch(e):
         global spotify
         print(stsearch.value)
         ui.notify("Searching...")
-        results = spotify.search(q=stsearch.value, type='track,artist', limit=30)
-        tracks_df = tracks_to_df(results['tracks']['items'])
-        artists_df = artists_to_df(results['artists']['items'])
-        track_grid.options['rowData'] = tracks_df
-        artist_grid.options['rowData'] = artists_df
+        try:    
+            results = spotify.search(q=stsearch.value, type='track,artist', limit=30)
+        except Exception as e:
+            ui.notify(e, type="warning")
+        track_grid.options['rowData'] = tracks_to_df(results['tracks']['items'])
+        artist_grid.options['rowData'] = artists_to_df(results['artists']['items'])
         track_grid.update()
         artist_grid.update()
-        
 
     async def do_rec(e):
         ui.notify("Getting recs...")
@@ -94,12 +111,21 @@ def page():
             for row in rows:
                 print(row)
                 seed_tracks += [row['trackid']]
-            print(seed_tracks)    
-            reccs = get_reccs(10, None, seed_tracks)
+            try:    
+                reccs =  spotify.recommendations(
+                    limit=10,
+                    seed_artists=None,
+                    seed_tracks=seed_tracks,
+                    seed_genre=None
+                    )
+            except Exception as e:
+                ui.notify(e, type="warning")
             rec_grid.options['rowData'] = tracks_to_df(reccs['tracks'])
             rec_grid.update()
         else:
             ui.notify('No rows selected.')
+
+            
 
     print("Running")
     with ui.row():
@@ -108,8 +134,9 @@ def page():
         ui.label('Switch mode:')
         ui.button('Dark', on_click=dark.enable)
         ui.button('Light', on_click=dark.disable)
+        ui.label('User: '+me['display_name'])
+        ui.button('Logout', on_click=logout)
 
-    spotify = get_spot()
     stsearch = ui.input("Search keywords").on('keydown.enter', onsearch)
     with ui.element('div').classes("w-full flex flex-wrap"):
         track_grid = ui.aggrid({
@@ -151,23 +178,30 @@ def page():
         try:    
             spotify.start_playback(device_id=device_id, context_uri='spotify:album:'+row['albumid'], offset={"uri":"spotify:track:"+row['trackid']}, position_ms=start*1000)
         except Exception as e:
-            ui.notify(e)
+            ui.notify(e, type="warning")
         
     def jump(ev):
-        res = spotify.current_playback()
-        vol = res['device']['volume_percent']
-        if vol != vol_slider.value:
-            vol_slider.value = vol
-        print(res)
-        spotify.seek_track(res['progress_ms']+30000, device_id=device_id)
+        try:
+            res = spotify.current_playback()
+            vol = res['device']['volume_percent']
+            if vol != vol_slider.value:
+                vol_slider.value = vol
+            print(res)
+            spotify.seek_track(res['progress_ms']+30000, device_id=device_id)
+        except Exception as e:
+            ui.notify(e, type="warning")
+
 
     def jumpback(ev):
-        res = spotify.current_playback()
-        vol = res['device']['volume_percent']
-        if vol != vol_slider.value:
-            vol_slider.value = vol
-        print(res)
-        spotify.seek_track(res['progress_ms']-30000, device_id=device_id)
+        try:
+            res = spotify.current_playback()
+            vol = res['device']['volume_percent']
+            if vol != vol_slider.value:
+                vol_slider.value = vol
+            print(res)
+            spotify.seek_track(res['progress_ms']-30000, device_id=device_id)
+        except Exception as e:
+            ui.notify(e, type="warning")
 
     rec_grid = ui.aggrid({
         'columnDefs': [
@@ -185,7 +219,10 @@ def page():
     def set_device(ev):
         global device_id
         device_id = ev.value
-        spotify.transfer_playback(device_id)
+        try:
+            spotify.transfer_playback(device_id)
+        except Exception as e:
+            ui.notify(e, type="warning")
 
     with ui.row().classes('w-full no-wrap'):
         ui.button("<< RR", on_click=jumpback).style('min-width: max-content')
@@ -205,5 +242,5 @@ def page():
         vol_slider = ui.slider(min=0, max=100, value=vol, step=1).on('update:model-value', lambda e: spotify.volume(e.args, device_id), throttle=1.0).classes('w-1/3')
 
 ui.link('Get started', page)
-ui.run(favicon='ᯤ', dark=None, title="Spotify Explorer",)
+ui.run(favicon='ᯤ', dark=None, title="Spotify Explorer", storage_secret='this is a private key no hacking')
 
